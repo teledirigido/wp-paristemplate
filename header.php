d9a24243767f5151053f6925f303aee763919cfb
Miguel Garrido <miguel@miguel-nk.local>
2022-04-26 09:41:17 +0200

    Header update for multilanguage support

--- header.php
+++ header.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
   <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="author" content="Miguel Garrido">
     <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXX-X"></script> -->
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
     <script>
       if (window.location.host==="domain.com" || window.location.host==="www.domain.com") {
         window.dataLayer = window.dataLayer || [];
